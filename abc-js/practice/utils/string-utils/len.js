7c54c0dd5eb6efd87686df83715b3d6f163b8209
Daniyara Galiyeva <gdaniyara@gmail.com>
2023-02-21 15:15:56 +0500

    Finished tasks len and compar

--- abc-js/practice/utils/string-utils/len.js
+++ abc-js/practice/utils/string-utils/len.js
@@ -1,6 +1,7 @@
 export function len(text) {
+    if (typeof text !== "string") throw Error ("argument must be type of string");
     let lenCount = 0;
-    for (let i = 0; text[i] == undefined; i+=1) {
+    for (let i = 0; text[i] !== undefined; i+=1) {
         lenCount+=1;
     } 
     return lenCount;
