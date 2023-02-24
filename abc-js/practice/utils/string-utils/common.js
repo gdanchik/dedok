2e9543d16e997385d3564f2dda05d8332b529a37
Daniyara Galiyeva <gdaniyara@gmail.com>
2023-02-24 16:48:15 +0500

    Исправлены все ошибки по код-ревью

--- abc-js/practice/utils/string-utils/common.js
+++ abc-js/practice/utils/string-utils/common.js
@@ -3,3 +3,7 @@
 export const complexText = "Hello world!!! It's terminator";
 
 export const ZERO_CODE_CHAR = String.fromCharCode(0);
+
+export function checkStringError(firstText, secondText) {
+    if (typeof secondText !== "string" || typeof firstText !== "string") throw Error ("argument must be type of string");
+};
